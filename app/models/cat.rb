b41e34d0a1f51bebdea8d0357ae835e3f8bc673f
Doug+Alex <Doug+Alex@appacademy.io>
2014-01-22 11:17:45 -0500

    Fixes typo in cats model. Adds CatsController. Adds routes for Cat. Adds index and show views for Cats.

--- app/models/cat.rb
+++ app/models/cat.rb
@@ -1,6 +1,6 @@
 class Cat < ActiveRecord::Base
   attr_accessible :age, :birthday, :color, :name, :sex
 
-  validates :age, :birth_date, :color, :name, :sex, :presence => true
+  validates :age, :birthday, :color, :name, :sex, :presence => true
   validates :sex, :length => { :is => 1 }
 end
