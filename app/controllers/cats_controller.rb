450f357f722f30aafbd324942908cb48470c6555
Doug+Alex <Doug+Alex@appacademy.io>
2014-01-22 14:56:57 -0500

    Adds #create to CatsController. Adds index, new, show views.

--- app/controllers/cats_controller.rb
+++ app/controllers/cats_controller.rb
@@ -5,6 +5,20 @@ class CatsController < ApplicationController
     render :index
   end
 
+  # def new
+#   end
+
+  def create
+    @cat = Cat.new(params[:cat])
+    if @cat.save
+      flash[:notice] = "New cat created"
+      redirect_to cat_url(@cat)
+    else
+      flash[:error] = "Invalid deets"
+      redirect_to new_cat_url
+    end
+  end
+
   def show
     @cat = Cat.find(params[:id])
 
