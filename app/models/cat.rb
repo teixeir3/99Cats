bbf8f8ab13cc9999f869abf7e4d72d65e5a82b12
Doug+Alex <Doug+Alex@appacademy.io>
2014-01-22 18:18:28 -0500

    Adds untested #make_pending, #not_overlapped, #overlapping_approved_requests, #overlapping_requests.

--- app/models/cat.rb
+++ app/models/cat.rb
@@ -3,4 +3,12 @@ class Cat < ActiveRecord::Base
 
   validates :age, :birthday, :color, :name, :sex, :presence => true
   validates :sex, :length => { :is => 1 }
+
+  has_many(
+    :rental_requests,
+    :class_name => 'CatRentalRequest',
+    :foreign_key => :cat_id,
+    :primary_key => :id,
+    :dependent => :destroy
+  )
 end
--- app/models/cat_rental_request.rb
+++ app/models/cat_rental_request.rb
@@ -4,4 +4,23 @@ class CatRentalRequest < ActiveRecord::Base
   validates :cat_id, :start_date, :end_date, :status, :presence => true
   validates :status, :inclusion => { :in => %w(PENDING APPROVED DENIED),
     :message => "%{value} is not a valid status"}
+
+    # Add association with cats
+    belongs_to(
+      :cat,
+      :class_name => 'Cat',
+      :foreign_key => :cat_id,
+      :primary_key => :id
+    )
+
+    def overlapping_requests
+      # it conflicts with ours if some other rental's start_date is before our end_date and after our start_date
+      # OR if their end date
+      CatRentalRequest.where("(cat_id = ?) AND NOT
+                      ((start_date > ?) OR (end_date < ?))", cat_id, end_date, start_date)
+    end
+
+    def overlapping_approved_requests
+      overlapping_requests.where("status = 'APPROVED'")
+    end
 end
