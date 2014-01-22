1c45d9ccd7b35a7b442d8b355f2fa9d11d091919
Doug+Alex <Doug+Alex@appacademy.io>
2014-01-22 15:32:31 -0500

    Edits new template. Adds #update and #edit to CatsController. Adds edit view.

--- app/controllers/cats_controller.rb
+++ app/controllers/cats_controller.rb
@@ -8,6 +8,12 @@ class CatsController < ApplicationController
   # def new
 #   end
 
+  def edit
+    @cat = Cat.find(params[:id])
+
+    render :edit
+  end
+
   def create
     @cat = Cat.new(params[:cat])
     if @cat.save
@@ -16,6 +22,17 @@ class CatsController < ApplicationController
     else
       flash[:error] = "Invalid deets"
       redirect_to new_cat_url
+    end
+  end
+
+  def update
+    @cat = Cat.find(params[:id])
+    if @cat.update_attributes(params[:cat])
+      flash[:notice] = "#{@cat.name}'s deets updated"
+      redirect_to cat_url(@cat)
+    else
+      flash[:error] = "Invalid deets"
+      redirect_to edit_cat_url(@cat)
     end
   end
 
